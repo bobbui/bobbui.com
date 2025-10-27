36680cdc5ac1f0d399ba1158999dc3b65cc5c40f
Bob Bui <bob@bui>
2025-10-27 02:23:30 -0700

    wip

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,24 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://bobbui.com/",
+  author: "Bob Bui",
+  profile: "https://bobbui.com/",
+  desc: "My personal website & blog | Writing about tech mainly AI | Where I share ideas, projects, and learnings",
+  title: "Bob Bui",
+  ogImage: "og-image.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 4,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showArchives: false,
+  showBackButton: true,
   editPost: {
     enabled: true,
-    text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    text: "Suggest Changes",
+    url: "https://github.com/bobbui/bobbui.com/edit/main/",
+    editLinkText: "Edit on GitHub",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "en",
+  timezone: "America/Los_Angeles", // IANA timezone format
 } as const;
