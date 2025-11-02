0b58788d8d38ef9bef8fbe2e1f313d15d84a32c4
Bob Bui <bob@bui>
2025-11-02 00:15:50 -0700

    add new social media link

--- src/navigation.ts
+++ src/navigation.ts
@@ -20,12 +20,28 @@ export const footerData = {
   links: [],
   secondaryLinks: [],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/bobbuitech' },
-    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/bobbui' },
-    { ariaLabel: 'Reddit', icon: 'tabler:brand-reddit', href: 'https://www.reddit.com/user/thangbui04' },
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/bobbui' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/bobbuitech' },
+    { ariaLabel: 'Reddit', icon: 'tabler:brand-reddit', href: 'https://www.reddit.com/user/thangbui04' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/bobbui' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/bobbuitech' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/bobbuitech' },
+    { ariaLabel: 'Threads', icon: 'tabler:brand-threads', href: 'https://www.threads.net/bobbuitech' },
+    { ariaLabel: 'Bluesky', icon: 'tabler:brand-bluesky', href: 'https://bobbui.bsky.social' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+  ],
+  footNote: `
+    Bob Bui· All rights reserved.
+  `,
+};
+export const footerData2 = {
+  links: [],
+  secondaryLinks: [],
+  socialLinks: [
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/bobbui' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/bobbuitech' },
+    { ariaLabel: 'Reddit', icon: 'tabler:brand-reddit', href: 'https://www.reddit.com/user/thangbui04' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/bobbui' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
